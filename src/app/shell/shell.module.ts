34d729d908aa4a49d86df067c8464dead08eb303
Alexander Bolshakov <alexander_bolshakov@hotmail.com>
2019-07-28 21:09:45 +0300

    Fixed centered components alignment.

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { FooterModule } from '../footer/footer.module';
 import { GalleryModule } from '../gallery/gallery';
 import { HeaderModule } from '../header/header.module';
+import { MaterialAndFlexModule } from '../shared/material-and-flex.module';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ShellComponent } from './shell.component';
@@ -12,6 +13,7 @@ import { ShellComponent } from './shell.component';
     GalleryModule,
     HeaderModule,
     FooterModule,
+    MaterialAndFlexModule,
     RouterModule
   ],
   declarations: [ShellComponent],
